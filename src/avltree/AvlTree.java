68f49737455299df9b38b412ccf9c8d4d3d695da
Vojin Pesalj <woyin95@gmail.com>
2020-06-28 17:45:57 +0200

    Added insert to avl tree

--- src/avltree/AvlTree.java
+++ src/avltree/AvlTree.java
@@ -11,7 +11,7 @@ public class AvlTree {
     this.root = root;
   }
 
-  private void rightRotate(Node node) {
+  private Node rightRotate(Node node) {
     Node leftChild = node.getChildren()[0];
     Node rightGrandChild = leftChild.getChildren()[1];
     leftChild.setChildren(new Node[] {leftChild.getChildren()[0], node});
@@ -19,20 +19,24 @@ public class AvlTree {
 
     increaseHeight(node);
     increaseHeight(leftChild);
+
+    return leftChild;
   }
 
-  private void leftRotate(Node node) {
+  private Node leftRotate(Node node) {
     Node rightChild = node.getChildren()[1];
     Node leftGrandChild = rightChild.getChildren()[0];
-    rightChild.setChildren(new Node[] {node, rightChild.getChildren()[0]});
+    rightChild.setChildren(new Node[] {node, rightChild.getChildren()[1]});
     node.setChildren(new Node[] {node.getChildren()[0], leftGrandChild});
 
     increaseHeight(node);
     increaseHeight(rightChild);
+
+    return rightChild;
   }
 
   private void increaseHeight(Node node) {
-    node.setHeight(max(node.getChildren()[0].getHeight(), node.getChildren()[1].getHeight()) + 1);
+    node.setHeight(max(height(node.getChildren()[0]), height(node.getChildren()[1])) + 1);
   }
 
   private int height(Node node) {
@@ -42,7 +46,63 @@ public class AvlTree {
     return node.getHeight();
   }
 
-  int max(int a, int b) {
+  private int max(int a, int b) {
     return (a > b) ? a : b;
   }
+
+  private int getBalance(Node node) {
+    if (node == null) {
+      return 0;
+    }
+
+    return height(node.getChildren()[0]) - height(node.getChildren()[1]);
+  }
+
+  public void print(Node node, String parent) {
+    if (node != null) {
+      System.out.println(parent + ": " + node.getValue());
+      print(node.getChildren()[0], node.getValue());
+      print(node.getChildren()[1], node.getValue());
+    }
+  }
+
+  public Node insert(Node parentNode, Node newNode) {
+    if (parentNode == null) {
+      return newNode;
+    }
+
+    Node leftChild = parentNode.getChildren()[0];
+    Node rightChild = parentNode.getChildren()[1];
+
+    if (newNode.getValue().compareTo(parentNode.getValue()) < 0) {
+      parentNode.setChildren(new Node[] {insert(leftChild, newNode), rightChild});
+    } else if (newNode.getValue().compareTo(parentNode.getValue()) > 0) {
+      parentNode.setChildren(new Node[] {leftChild, insert(rightChild, newNode)});
+    } else {
+      return parentNode;
+    }
+
+    increaseHeight(parentNode);
+
+    int balance = getBalance(parentNode);
+
+    if (balance > 1 && newNode.getValue().compareTo(leftChild.getValue()) < 0)
+      return rightRotate(parentNode);
+
+    if (balance < -1 && newNode.getValue().compareTo(rightChild.getValue()) > 0)
+      return leftRotate(parentNode);
+
+    if (balance > 1 && newNode.getValue().compareTo(leftChild.getValue()) > 0) {
+      parentNode.setChildren(new Node[] {leftRotate(leftChild), rightChild});
+      return rightRotate(parentNode);
+    }
+
+    if (balance < -1 && newNode.getValue().compareTo(rightChild.getValue()) < 0) {
+      parentNode.setChildren(new Node[] {leftChild, rightRotate(rightChild)});
+      return leftRotate(parentNode);
+    }
+
+    return parentNode;
+  }
+
 }
--- src/avltree/Main.java
+++ src/avltree/Main.java
@@ -2,7 +2,14 @@ package avltree;
 
 public class Main {
   public static void main(String[] args) {
+    AvlTree tree = new AvlTree();
 
+    tree.setRoot(new Node("a"));
+    tree.setRoot(tree.insert(tree.getRoot(), new Node("b")));
+    tree.setRoot(tree.insert(tree.getRoot(), new Node("c")));
+    tree.setRoot(tree.insert(tree.getRoot(), new Node("d")));
+
+    tree.print(tree.getRoot(), "Root");
   }
 
 }
